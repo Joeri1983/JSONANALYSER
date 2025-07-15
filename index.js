2fccb71dcc0b3f076260e332761ea168216490bc
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2025-07-15 13:10:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const html = `
   <title>JSON Key Explorer</title>
 </head>
 <body>
-  <h1>JSON Key Explorer</h1>
-  <textarea id="jsonInput" rows="14" cols="70" placeholder='Enter JSON object or array of objects'></textarea><br>
+  <h1>Paste the JSON data into the textbox below</h1>
+  <textarea id="jsonInput" rows="40" cols="400" placeholder='Enter JSON object or array of objects'></textarea><br>
   <button onclick="compute()">Show connectors</button>
 
   <h3>Connection Names:</h3>
